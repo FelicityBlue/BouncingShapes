65a7bdd010598e497e005e4af8ffcafed65e85e0
FelicityBlue <nb29691@umbc.edu>
2024-01-11 04:11:12 -0500

    use classes to get shapes in vectors & get rectangles to move correctly

--- BoundingShapes.cpp
+++ BoundingShapes.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <SFML/Graphics.hpp>
+#include "BouncingShapes.h";
 
 std::vector <std::string> splitWords(std::string line, std::string delimiter){
     std::vector<std::string> words;
@@ -17,25 +15,27 @@ std::vector <std::string> splitWords(std::string line, std::string delimiter){
 
 sf::Color getColor(std::vector <std::string> words) {
     // 6 7 8
-    if (words.at(1).at(0) == 'C') {
-        sf::Color c(std::stoi(words.at(6)), stoi(words.at(7)), stoi(words.at(8)));
+    if (words.at(1).at(0) == 'C' || words.at(1).at(0) == 'R') {
+        sf::Color c(std::stof(words.at(6)), stof(words.at(7)), stof(words.at(8)));
         return c;
     }
 
-    // 7 8 9
-    if (words.at(1).at(0) == 'R') {
-        sf::Color c(std::stoi(words.at(7)), stoi(words.at(8)), stoi(words.at(9)));
+    if (words.at(0) == "Font") {
+        sf::Color c(std::stof(words.at(words.size()-3)), std::stof(words.at(words.size() - 2 )), std::stof(words.at(words.size() - 1)));
         return c;
     }
 }
 
 int main()
 {
-    std::vector <sf::RectangleShape> rects;
-    std::vector <sf::CircleShape> cirs;
+    std::vector <RECT> rects;
+    std::vector <CIRCLE> cirs;
     std::vector <std::string> words;
+    std::vector <sf::Text> texts;
+    sf::Text text;
     std::string line;
-
+    float xVelocity = 0.0, yVelocity = 0.0, xPos = 0.0, yPos = 0.0;
+    sf::RenderWindow window;
     // open file
     std::ifstream configFile("C:\\Users\\Felicity\\source\\repos\\BoundingShapes\\configs\\config.txt");
     if (!configFile.is_open()) {
@@ -46,24 +46,80 @@ int main()
     while(std::getline(configFile, line)) {
         words = splitWords(line, " ");
 
+        std::cout << std::endl;
         if (words.at(0) == "Circle") {
-            sf::CircleShape cir;
-            cir.setFillColor(getColor(words));
-            cir.setRadius(std::stoi(words.at(words.size() - 1)));
-            cir.setPosition(sf::Vector2f(std::stoi(words.at(2)), std::stoi(words.at(3))));
-            cir.move(sf::Vector2f(std::stoi(words.at(4)), std::stoi(words.at(5))));
-            cirs.push_back(cir);
+           // sf::Color color, float radius, sf::Vector2f pos, float x_vel, float y_vel
+            cirs.push_back((CIRCLE(getColor(words), std::stof(words.at(words.size() - 1)),
+                (sf::Vector2f(std::stof(words.at(2)), 
+                    std::stof(words.at(3)))), std::stoi(words.at(4)), std::stoi(words.at(5)))));
         }
         else if (words.at(0) == "Rectangle") {
-            sf::RectangleShape rect;
-            rect.setFillColor(getColor(words));
-            rect.setSize(sf::Vector2f(std::stoi(words.at(words.size() - 2)), std::stoi(words.at(words.size() - 1))));
-            rect.setPosition(sf::Vector2f(std::stoi(words.at(2)), std::stoi(words.at(3))));
-            rect.move(sf::Vector2f(std::stoi(words.at(4)), std::stoi(words.at(5))));
-            rects.push_back(rect);
+            // sf::Color color, sf::Vector2f size, sf::Vector2f pos, float x_vel, float y_vel
+            rects.push_back(RECT(getColor(words), 
+                sf::Vector2f(std::stof(words.at(9)), std::stof(words.at(10))), 
+                sf::Vector2f(std::stof(words.at(2)), std::stof(words.at(3))), 
+                std::stof(words.at(4)), std::stof(words.at(5))));
         }
-
+        // set up window
+        else if (words.at(0) == "Window") {
+            window.create(sf::VideoMode(std::stoi(words.at(1)), std::stoi(words.at(2))), "Bounding Shapes");
+            
+        }
+        // set up fonts
+        else if (words.at(0) == "Font") {
+            sf::Font techFont;
+            if (!techFont.loadFromFile("C:\\Users\\Felicity\\source\\repos\\AimTrainer\\8-bit-operator\\8bitOperatorPlus8-Regular.ttf")) {
+                std::cerr << ("Error: Cannot load the font");
+                return -1;
+            }
+            text.setFont(techFont);
+            text.setCharacterSize(std::stoi(words.at(2)));
+            text.setFillColor(getColor(words));
+            
+        }
     }
+
+    sf::Event event;
+
+    while (window.isOpen()) {
+        // render
+        window.clear(sf::Color::Black);
+       
+         for (auto& r : rects) {
+            
+            xPos = r.getXPos();
+            yPos = r.getYPos();
+            xVelocity = r.getXVelocity();
+            yVelocity = r.getYVelocity();
+
+            std::cout << " old x: " << xPos << " y: " << yPos <<  std::endl;
+        
+            if (xPos >= (window.getSize().x - r.rect.getSize().x) || xPos <= 0) {
+                r.setXVelocity(-xVelocity);
+            }
+            if (yPos >= (window.getSize().y - r.rect.getSize().y) || yPos <= 0) {
+                r.setYVelocity(-yVelocity);
+            }
+            xPos += r.getXVelocity();
+            yPos += r.getYVelocity();
+
+            std::cout << " new x: " << xPos << " y: " << yPos << std::endl;
+
+            r.rect.setPosition(sf::Vector2f(xPos, yPos));
+            window.draw(r.rect);
+            
+         }
+
+        
+
+        
+        window.display();
+    }
+    
+    // TEST
+    std::cout << "The size of the circle vector is " << cirs.size() << std::endl;
+    std::cout << "The size of the rectangle vector is " << rects.size() << std::endl;
+    
 
     configFile.close();
     return 0;
