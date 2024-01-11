f0d0b1d3e2337d4a1a07de97e6253ab24ab37b1a
FelicityBlue <nb29691@umbc.edu>
2024-01-11 01:53:34 -0500

    add class for rectangle

--- BouncingShapes.h
+++ BouncingShapes.h
@@ -22,3 +22,26 @@ private:
     float xVelocity;
     float yVelocity;
 };
+
+class RECT {
+public:
+    sf::RectangleShape rect;
+    RECT(sf::Color color, sf::Vector2f size, sf::Vector2f pos, float x_vel, float y_vel) {
+
+        rect.setFillColor(color);
+        rect.setSize(size);
+        rect.setPosition(pos);
+        xVelocity = x_vel;
+        yVelocity = y_vel;
+        
+    }
+    float getXVelocity() {
+        return xVelocity;
+    }
+    float getYVelocity() {
+        return yVelocity;
+    }
+private:
+    float xVelocity;
+    float yVelocity;
+};
