c93fda651c7fd269575b15f9a8cf7541fe1fb39d
FelicityBlue <nb29691@umbc.edu>
2024-01-11 04:47:49 -0500

    add funcs in classes to get position

--- BouncingShapes.h
+++ BouncingShapes.h
@@ -5,18 +5,35 @@
 class CIRCLE {
 public:
     sf::CircleShape cir;
-    CIRCLE(sf::Color color, float radius, sf::Vector2f pos, float x_vel, float y_vel) {
+    CIRCLE(sf::Color color, float radius, sf::Vector2f pos, float xVel, float yVel) : xVelocity(xVel), yVelocity(yVel) {
         cir.setFillColor(color);
         cir.setRadius(radius);
         cir.setPosition(pos);
-        xVelocity = x_vel;
-        yVelocity = y_vel;
+        xVelocity = xVel;
+        yVelocity = yVel;
     }
+    // setters
+    void setXVelocity(float newXVel) {
+        xVelocity = newXVel;
+    }
+    void setYVelocity(float newYVel) {
+        yVelocity = newYVel;
+    }
+    void setPos(float x, float y) {
+        cir.setPosition(sf::Vector2f(x, y));
+    }
+    // getters
     float getXVelocity() {
         return xVelocity;
     }
     float getYVelocity() {
         return yVelocity;
+    }
+    float getXPos() {
+        return cir.getPosition().x;
+    }
+    float getYPos() {
+        return cir.getPosition().y;
     }
 private:
     float xVelocity;
@@ -26,20 +43,31 @@ private:
 class RECT {
 public:
     sf::RectangleShape rect;
-    RECT(sf::Color color, sf::Vector2f size, sf::Vector2f pos, float x_vel, float y_vel) {
+    RECT(sf::Color color, sf::Vector2f size, sf::Vector2f pos, float xVel, float yVel): xVelocity(xVel), yVelocity(yVel) {
 
         rect.setFillColor(color);
         rect.setSize(size);
         rect.setPosition(pos);
-        xVelocity = x_vel;
-        yVelocity = y_vel;
-        
     }
+    // setters
+    void setXVelocity(float newXVel) {
+        xVelocity = newXVel;
+    }
+    void setYVelocity(float newYVel) {
+        yVelocity = newYVel;
+    }
+    // getters
     float getXVelocity() {
         return xVelocity;
     }
     float getYVelocity() {
         return yVelocity;
+    }
+    float getXPos() {
+        return rect.getPosition().x;
+    }
+    float getYPos() {
+        return rect.getPosition().y;
     }
 private:
     float xVelocity;
